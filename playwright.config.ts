2c24597c852057c66c0c5a96670f2c107651493d
bryankiansarmen <sarmenbryankian@gmail.com>
2026-08-14 15:08:58 +0800

    chore(test): move e2e tests into tests/e2e directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: "./tests/e2e",
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
